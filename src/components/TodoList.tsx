69d9b1b172d9dbb8ef0c3c592b217431318ebb8f
ACCESA\alexandru.kelemen <alexandru.kelemen@accesa.eu>
2022-11-11 14:03:29 +0200

    clean up

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import { Todo } from "../interfaces/Todo";
+
+interface Props {
+  todos: Todo[];
+}
+
+const TodoList = ({ todos }: Props) => {
+  return (
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <>
+          <TodoListItem
+            todo={{
+              task: todo.task,
+              finished: todo.finished,
+            }}
+          />
+        </>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
